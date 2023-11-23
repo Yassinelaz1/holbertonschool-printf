105bc4e423e1605c2fb8c5b3940671d73154aca5
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-11-23 19:11:05 +0100

    function.c

--- function.c
+++ function.c
@@ -5,9 +5,8 @@
  */
 int p_string(va_list args)
 {
-	int byte = 0;
-	int i = 0;
-	char *string = va_arg(args,char *);
+	int i;
+	char *string = va_arg(args, char *);
 
 	if (args == NULL)
 	{
@@ -18,19 +17,18 @@ int p_string(va_list args)
 		for (i = 0; i != '\0'; i++)
 		{
 			_putchar(string[i]);
-			byte++;
 		}
-		return (byte);
+		return (0);
 	}
 }
 /**
  * p_char-print char
  * @args:argument
-*/
+ */
 int p_char(va_list args)
 {
 	char c = va_arg(args, int);
 
 	_putchar(c);
-	return (0);
+	return (1);
 }
