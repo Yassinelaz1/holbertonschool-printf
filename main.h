c974d0bdd526c1f5a0853767a30635ae13a7ce76
EyaKadri <eyakaad@gmail.com>
2023-11-24 13:15:32 +0100

    printf file

--- main.h
+++ main.h
@@ -4,36 +4,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/*
- * struct func-structure for printf
+/**
+ * struct func - structure for printf
  * @func: pointer to characteres
  * @function: function pointer that print fucntions
- */
-typedef struct func
-{
-	char func;
-	int (*function)(va_list);
-} get_func;
-
-int _putchar(char c);
-int (*get_function(char s))(va_list);
-int p_char(va_list args);
-int p_percent(va_list);
-int p_string(va_list args);
-int _printf(const char *format, ...);
-
-#endif
-#ifndef _PRINTF_FUNCTIONS_
-#define _PRINTF_FUNCTIONS_
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-/*
- * struct func-structure for printf
- * @func: pointer to characteres
- * @function: function pointer that print fucntions
- */
+*/
 typedef struct func
 {
 	char func;
